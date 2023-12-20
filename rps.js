a40e54469ab45293cdad55d72f6979ad1001c5f4
zkhan04 <khanzayd11@gmail.com>
2023-12-19 21:53:15 -0600

    Got it working, will refactor now

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+// randomly returns rock, paper or scissors
 function computerChoice(){
     let random_number = Math.floor(Math.random() * 3);
     if (random_number === 0){
@@ -9,59 +10,98 @@ function computerChoice(){
     }
 }
 
-
-
-function playRound() {
-    let playerSelection = prompt("Enter your choice.");
-    playerSelection = playerSelection.toLowerCase();
+// uses the player input as a parameter and plays the round
+function playRound(playerSelection) {
+    // declares the result of each round
+    const result_declaration = document.querySelector('#round-result');
 
     let computerSelection;
     computerSelection = computerChoice();
 
-    while (playerSelection === computerSelection) {
-        playerSelection = prompt("Tie! Roll again.");
-        computerSelection = computerChoice();
-    }
+    let playerWin = new Event('playerWin');
+    let computerWin = new Event('computerWin');
 
-    if (playerSelection === 'rock') {
+    if (playerSelection === computerSelection){
+        result_declaration.textContent = ("Tie! Roll again.")
+    } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            console.log("You lose! Paper beats rock");
-            return false;
+            result_declaration.textContent = "You lose! Paper beats rock";
+            computer.dispatchEvent(computerWin);
         } else {
-            console.log("You win! Rock beats scissors");
-            return true;
+            result_declaration.textContent = "You win! Rock beats scissors";
+            player.dispatchEvent(playerWin);
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            console.log("You win! Paper beats rock");
-            return true;
+            result_declaration.textContent = "You win! Paper beats rock";
+            player.dispatchEvent(playerWin);
         } else {
-            console.log("You lose! Scissors beats paper");
-            return false;
+            result_declaration.textContent = "You lose! Scissors beats paper";
+            computer.dispatchEvent(computerWin);
         }
     } else {
         if (computerSelection === 'paper') {
-            console.log("You win! Scissors beats paper");
-            return true;
+            result_declaration.textContent = "You win! Scissors beats paper";
+            player.dispatchEvent(playerWin);
         } else {
-            console.log("You lose! Rock beats scissors");
-            return false;
+            result_declaration.textContent = "You lose! Rock beats scissors";
+            computer.dispatchEvent(computerWin);
         }
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+// the three buttons representing rock, paper, scissors
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
-    for (let i = 0; i < 5; i++) {
-        if (playRound()) {
-            playerScore++;
-        } else {
-            computerScore++;
-        }
-        console.log("Player: " + playerScore + " Computer: " + computerScore);
+// when any of the buttons are clicked, run playRound()
+rock.addEventListener('click', () => playRound('rock'));
+paper.addEventListener('click', () => playRound('paper'));
+scissors.addEventListener('click', () => playRound('scissors'))
+
+// variables storing player score and computer score
+let playerScore = 0;
+let computerScore = 0;
+
+// references to text fields displaying player and computer score
+const player = document.querySelector('#player-score');
+const computer = document.querySelector('#computer-score');
+
+// the player score field listens for a playerWin event.
+player.addEventListener('playerWin', (e) => {
+    playerScore += 1;
+    e.target.textContent = "Player: " + playerScore;
+
+    // if the playerscore hits 5, send a game-end event to gameResult indicating that the player won
+    if (playerScore === 5) {
+        let playerVictory = new CustomEvent('gameEnd', {
+            detail: {
+                winner: 'Player'
+            }
+        });
+        gameResult.dispatchEvent(playerVictory);
     }
-}
+})
 
-game();
+// computer score field listens for a computerWin event.
+computer.addEventListener('computerWin', (e) => {
+    computerScore += 1;
+    e.target.textContent = "Computer: " + computerScore;
+
+    // if the computer score hits 5, send a game-end event to gameResult indiciating that the computer won
+    if (computerScore === 5) {
+        let computerVictory = new CustomEvent('gameEnd', {
+            detail: {
+                winner: 'Computer'
+            }
+        });
+        gameResult.dispatchEvent(computerVictory);
+    }
+})
+
+// when someone wins, display it in the gameResult field
+const gameResult = document.querySelector('#game-result');
+gameResult.addEventListener('gameEnd', (e) => {
+    gameResult.textContent = e.detail.winner + ' wins!';
+})
